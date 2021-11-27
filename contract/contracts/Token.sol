27bd0d21dc7fa461e1865c4994951dd295ed5164
Wayfarer <iamnickhil@users.noreply.github.com>
2021-11-27 15:40:54 +0530

    Update Token.sol

--- contract/contracts/Token.sol
+++ contract/contracts/Token.sol
@@ -2,7 +2,7 @@ pragma solidity 0.4.19;
     import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 
     contract Token is MintableToken {
-        string public name = "TESTTOKEN";
-        string public symbol = "TTT";
+        string public name = "Procyon";
+        string public symbol = "PCY";
         uint8 public decimals = 18;
     }
